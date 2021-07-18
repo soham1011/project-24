2232610d6a8d501a7ccd8c5cff90c3a66a7a569a
soham1011 <72206422+soham1011@users.noreply.github.com>
2021-07-18 13:07:07 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+class Paper{
+
+
+    constructor(x,y,radius){
+
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+
+            
+            }
+       this.body = Bodies.circle(x,y,radius,options);
+       this.radius=radius;
+     //  this.image= loadImage("images/paper.png")
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        fill("red")
+        push ()
+        translate(pos.x,pos.y);
+        rotate(angle)
+    
+        ellipseMode(RADIUS)
+       
+      ellipse(0,0,this.radius);
+        pop ();
+    }
+}
